7e9c2aba2fadb4ad9129966d63460dfd19ff7f50
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-05-14 12:32:40 +0530

    Update Youtube.py

--- AviaxMusic/platforms/Youtube.py
+++ AviaxMusic/platforms/Youtube.py
@@ -16,7 +16,7 @@ from urllib3.util.retry import Retry
 from py_yt import VideosSearch
 from AviaxMusic import LOGGER
 from AviaxMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from AnonXMusic.utils.formatters import 
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 logger = LOGGER(__name__)
