5ff71116142767742a664cf1f81a4eca5a14a0d4
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-05-20 12:33:28 +0530

    Update Youtube.py

--- AviaxMusic/platforms/Youtube.py
+++ AviaxMusic/platforms/Youtube.py
@@ -10,7 +10,7 @@ import aiohttp
 
 API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
 
-API_KEY = os.environ.get("SHRUTI_API_KEY", "https://api.shrutibots.site") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
+API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsAlSpfeG7JItQmuoxCqKd") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
