d13ced168dfb5860e1a10d938a8a283bedeed69a
istkharroohi78 <istkharroohi@gmail.com>
2026-07-15 00:36:46 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -32,7 +32,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style=ButtonStyle.SUCCESS),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/+M4PHkrx84_ZmNjM1", style=ButtonStyle.SUCCESS),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/TuneBots", style=ButtonStyle.SUCCESS),
         ],
     ]
     return buttons
