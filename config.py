7e27bd4ecd85a1034fe39042e7107eca796479cc
istkharroohi78 <istkharroohi@gmail.com>
2026-07-15 08:52:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/istkharroohi78/Testop",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
